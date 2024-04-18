c8d008741c2cb9f8fc863be4af260620fe3a26d3
gb-mobile <110418063+gb-mobile@users.noreply.github.com>
2024-04-17 20:54:11 -0400

    Fixed A-Z-Blank selection

--- data/mobile/charpools.asm
+++ data/mobile/charpools.asm
@@ -2,7 +2,6 @@
 	const CHARPOOL_0_TO_9
 	const CHARPOOL_A_TO_Z
 	const CHARPOOL_A_TO_9
-	const CHARPOOL_A_TO_Z_BLANK
 	const CHARPOOL_L
 	const CHARPOOL_V
 	const CHARPOOL_M
@@ -13,6 +12,9 @@
 	const CHARPOOL_7
 	const CHARPOOL_8
 	const CHARPOOL_9
+	const CHARPOOL_A_TO_Z_2
+	const CHARPOOL_A_TO_Z_BLANK	
+	const CHARPOOL_BLANK
 	const CHARPOOL_DASH
 
 
@@ -22,7 +24,6 @@ Zipcode_CharPools:
 	dwb Zipcode_CharPool_0to9,       10 ; CHARPOOL_0_TO_9
 	dwb Zipcode_CharPool_AtoZ,       26 ; CHARPOOL_A_TO_Z
 	dwb Zipcode_CharPool_Ato9,       36 ; CHARPOOL_A_TO_9
-	dwb Zipcode_CharPool_AtoZ_Blank, 27 ; CHARPOOL_BLANK_A_TO_Z
 	dwb Zipcode_CharPool_L,           1 ; CHARPOOL_L
 	dwb Zipcode_CharPool_V,           1 ; CHARPOOL_V
 	dwb Zipcode_CharPool_M,           1 ; CHARPOOL_M
@@ -33,9 +34,11 @@ Zipcode_CharPools:
 	dwb Zipcode_CharPool_7,           1 ; CHARPOOL_7
 	dwb Zipcode_CharPool_8,           1 ; CHARPOOL_8
 	dwb Zipcode_CharPool_9,           1 ; CHARPOOL_9
+	dwb Zipcode_CharPool_AtoZ_2,     26 ; CHARPOOL_A_TO_Z_2
+	dwb Zipcode_CharPool_AtoZ_Blank, 27 ; CHARPOOL_BLANK_A_TO_Z
+	dwb Zipcode_CharPool_Blank,       1 ; CHARPOOL_BLANK
 	dwb Zipcode_CharPool_Dash,        1 ; CHARPOOL_DASH
 
-Zipcode_CharPool_AtoZ_Blank:
 Zipcode_CharPool_Ato9:
 Zipcode_CharPool_AtoZ:
 	db "ABC"
@@ -51,8 +54,6 @@ Zipcode_CharPool_S:
 	db "STU"
 Zipcode_CharPool_V:
 	db "VWXYZ"
-Zipcode_CharPool_Blank:
-	db " "	
 Zipcode_CharPool_0to9:
 	db "0123"
 Zipcode_CharPool_4:
@@ -63,5 +64,10 @@ Zipcode_CharPool_8:
 	db "8"
 Zipcode_CharPool_9:
 	db "9"
+Zipcode_CharPool_AtoZ_Blank:
+Zipcode_CharPool_AtoZ_2:
+	db "ABCDEFGHIJKLMNOPQRSTUVWXYZ"	
+Zipcode_CharPool_Blank:
+	db " "	
 Zipcode_CharPool_Dash:
 	db "-"
